4dd9f06fa6e9b374a918cae116d9e56adc36d33e
AllenRodger22 <91630631+AllenRodger22@users.noreply.github.com>
2022-03-12 02:39:40 -0300

    Boxes added but it's glitched
    we can move the boxes but it's pretty glitched. I'm still thinking if it should be done with RayCast2D or collisions system.

--- car.gd
+++ car.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
 var tile_size = 32
-var SPEED = 64
+var SPEED = 1
+signal PUSH(dir)
 var inputs = {"right": Vector2.RIGHT,
 			"left": Vector2.LEFT,
 			"up": Vector2.UP,
@@ -16,15 +17,23 @@ func _physics_process(delta):
 	
 	
 func move():
+	var dir 
 	var velocity = Vector2.ZERO
 	if Input.is_action_just_pressed('ui_left'):
+		dir = 'left'
 		velocity = inputs['left'] * SPEED * tile_size
 	elif Input.is_action_just_pressed('ui_right'):
+		dir = 'right'
 		velocity = inputs['right'] * SPEED * tile_size
 	elif Input.is_action_just_pressed('ui_up'):
+		dir = 'up'
 		velocity = inputs['up'] * SPEED * tile_size
 	elif Input.is_action_just_pressed('ui_down'):
+		dir = 'down'
 		velocity = inputs['down'] * SPEED * tile_size
 	else:
 		velocity = Vector2.ZERO
-	move_and_slide(velocity)
+	var collision = move_and_collide(velocity)
+	if collision:
+		if collision.collider.name == 'Box':
+			emit_signal("PUSH",dir)
