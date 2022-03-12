a88f912ebb39307c5644daefdb372c10023eaa66
AllenRodger22 <91630631+AllenRodger22@users.noreply.github.com>
2022-03-12 01:53:15 -0300

    changes on movements
    now the collisions on wall is working! also we are using move and slide()

--- car.gd
+++ car.gd
@@ -1,31 +1,30 @@
 extends KinematicBody2D
 
 var tile_size = 32
+var SPEED = 64
 var inputs = {"right": Vector2.RIGHT,
 			"left": Vector2.LEFT,
 			"up": Vector2.UP,
 			"down": Vector2.DOWN}
 
-
 func _ready():
-	position = position.snapped(Vector2.ONE * tile_size)
-	position += Vector2.ONE * tile_size/2
+	pass
 	
 	
 func _physics_process(delta):
 	move()
 	
 	
-
 func move():
-	var dir 
-	if Input.is_action_just_pressed("ui_left"):
-		dir = 'left'
-	elif Input.is_action_just_pressed("ui_right"):
-		dir = 'right'
-	elif Input.is_action_just_pressed("ui_up"):
-		dir = 'up'
-	elif Input.is_action_just_pressed("ui_down"):
-		dir = 'down'
-	if dir != null:
-		position += inputs[dir] * tile_size
+	var velocity = Vector2.ZERO
+	if Input.is_action_just_pressed('ui_left'):
+		velocity = inputs['left'] * SPEED * tile_size
+	elif Input.is_action_just_pressed('ui_right'):
+		velocity = inputs['right'] * SPEED * tile_size
+	elif Input.is_action_just_pressed('ui_up'):
+		velocity = inputs['up'] * SPEED * tile_size
+	elif Input.is_action_just_pressed('ui_down'):
+		velocity = inputs['down'] * SPEED * tile_size
+	else:
+		velocity = Vector2.ZERO
+	move_and_slide(velocity)
