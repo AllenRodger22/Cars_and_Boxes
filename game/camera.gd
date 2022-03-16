bb55f89fb9200d0044117fd7a764e20fe2855373
AtomiKune <93685179+AtomiKune@users.noreply.github.com>
2022-03-16 23:15:59 +0100

    added smooth camera, changed project settings

---
+++ game/camera.gd
@@ -0,0 +1,15 @@
+extends Camera2D
+
+var tracking
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	var cars = get_tree().get_nodes_in_group("cars")
+	print(cars)
+	for i in cars:
+		if i.is_active: tracking = i
+	
+	if tracking != null:
+		global_position = lerp(global_position, tracking.get_node("CollisionShape2D").global_position, 2 * delta)
